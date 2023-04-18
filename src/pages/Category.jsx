8356673ebaf563e2e4fcf14902488a6531280953
Joosung Jin <joosung.jin@samsung.com>
2023-04-19 07:33:17 +0900

    work0419

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,6 +4,7 @@ import { collection, getDocs, query, where, orderBy, limit, startAfter, QuerySna
 import { db } from "../firebase.config"
 import { toast } from "react-toastify"
 import Spinner from "../components/Spinner"
+import ListingItem from "../components/ListingItem"
 
 function Category() {
   const [listing, setListings] = useState(null)
@@ -61,7 +62,8 @@ function Category() {
         <main>
           <ul className="categoryListings">
             {listing.map((listing)=> {
-              <h3 key={listing.id}>{listing.data.name}</h3>
+              <ListingItem listing={listing.data} 
+                id={listing.id} key={listing.id}/>
             })}
           </ul>
         </main>
